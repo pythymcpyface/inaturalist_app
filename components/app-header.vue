4bf40aeef338251683ed82c4f8509cb366a885e5
pythymcpyface <gibby5000@hotmail.co.uk>
2023-01-12 10:00:00 +0000

    feat: setup ui components and tailwind css

---
+++ components/app-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="flex justify-between items-start md:items-center mt-5">
+    <div class="flex items-center md:space-x-12">
+      <div class="hidden md:block">
+        <NuxtLink
+          to="/"
+          class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800"
+        >
+          Observations Predictor
+        </NuxtLink>
+      </div>
+    </div>
+
+    <ClientOnly>
+      <ColorModeSelector />
+    </ClientOnly>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mx-auto max-w-8xl">
+    <AppHeader />
+    <div class="flex flex-col">
+      <main class="p-2 mt-10">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body, .highcharts-background {
+  @apply fill-white dark:fill-gray-900 !important;
+}
+
+text {
+  @apply dark:fill-white fill-gray-900 !important;
+}
+
+
+</style>
